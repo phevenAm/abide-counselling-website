6ba17d61dd42daab329f7767a68f273719128629
stephen M <stephen.missah@aventri.com>
2022-11-26 22:29:14 +0000

    faqs styled + intersection observor added / not working

--- script/main.js
+++ script/main.js
@@ -1,70 +1,63 @@
-const nav = document.getElementById('nav');
-const header = document.getElementById('header');
-
-const burgermenu = document.getElementById('burgerMenu');
-
-function closeNav() {
-	nav.classList.add('closed');
-	burgermenu.classList.add('closed');
-}
-
-burgermenu.addEventListener('click', () => {
-	nav.classList.toggle('closed');
-	burgermenu.classList.toggle('closed');
-});
-
-const allSections = Array.from(document.querySelectorAll('section'));
 
 
-window.addEventListener('scroll', (e) => {
-	e.preventDefault();
-	console.log(`Scroll position is currently: ${scrollY}. Documents fix height is ${document.body.offsetHeight}`);
-	let scroll = scrollY;
+$(document).ready(function () {
 
-	//console.log(scroll + 'is the scroll')
-
-	//console.log(allSections);
-
-	allSections.forEach((section) => {
-		//if(scrollY >=  - section.offsetTop && scrollY <= (document.body.offsetHeight - (section.offsetTop + window.innerHeight))) {
-		//	console.log(scrollY, section.offsetTop, document.body.offsetHeight);
-		//	allSections.forEach(section =>  section.classList.remove('active'));
-		//	section.classList.add('active');
-		//}
-
-		let sectionHeight = section.clientHeight;
-
-		//!test 2
-		if(scrollY >= section.offsetTop && section.offsetTop <= (document.body.offsetHeight - scrollY)) {
-			//console.log(scrollY, section.offsetTop, document.body.offsetHeight);
-			allSections.forEach(section =>  section.classList.remove('active'));
-			section.classList.add('active');
-		}
-
-		//! test 3
-		 
-			//console.log(scrollY, section.offsetTop, document.body.offsetHeight);
-			//allSections.forEach(section =>  {
-			//	section.classList.remove('active')
-			//	if(scrollY >= (section.offsetTop + section.offsetHeight) && scrollY <= (scrollY + (section.offsetHeight))) {
-			//		section.classList.add('active');
-			//	}
-			//});
-		
-	});
-});
+	const nav = document.getElementById('nav');
+	const header = document.getElementById('header');
+	const burgermenu = document.getElementById('burgerMenu');
 
 
-
-window.addEventListener('scroll', () => {
-	console.log('resize')
-	if(window.scrollY >= '100' || window.innerWidth < '1000') {
-		header.classList.add('shrink');
-	} else {
-		header.classList.remove('shrink');
+	//! Navbar toggle
+	function closeNav() {
+		nav.classList.add('closed');
+		burgermenu.classList.add('closed');
 	}
-})
 
-if(window.innerWidth < '900') {
-	header.classList.add('shrink');
-}
+	burgermenu.addEventListener('click', () => {
+		nav.classList.toggle('closed');
+		burgermenu.classList.toggle('closed');
+	});
+
+	const allSections = Array.from(document.querySelectorAll('section'));
+
+
+	//! Header shrink
+	$(window).on('scroll', () => {
+		if (window.scrollY >= '100' || window.innerWidth < '1000') {
+			header.classList.add('shrink');
+		} else {
+			header.classList.remove('shrink');
+		}
+	})
+
+	if (window.innerWidth < '900') {
+		header.classList.add('shrink');
+	}
+
+
+	//!intersection Observer
+
+	function interLoad(){
+		allSections.forEach((section) => {
+
+			observer = new IntersectionObserver((entries) => {
+				console.log(entries);
+
+				entries.forEach((entry) =>{
+					if(entry.intersectionRatio > 0.1 && entries.intersectionRatio <= 1) {
+						entry.target.classList.add('active');
+					} else {
+						entry.target.classList.remove('active');
+					}
+				})
+			});
+
+			allSections.forEach(section => observer.observe(section))
+		});
+	}
+
+	interLoad();
+
+
+});
+
