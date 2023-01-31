b1917d172d98e16d262926b15bc4d9ef9f039d5e
stephen M <stephen.missah@aventri.com>
2023-01-31 13:08:50 +0000

    removed node package as Im NOT building app

---
+++ index.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-analytics.js";
+import { getFirestore, collection } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-store.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDwTLDSdBgValD8APWqd2sv37Yat3dia0c",
+    authDomain: "rosie-s-counselling.firebaseapp.com",
+    projectId: "rosie-s-counselling",
+    storageBucket: "rosie-s-counselling.appspot.com",
+    messagingSenderId: "556608009766",
+    appId: "1:556608009766:web:263198fdc072e83307d8e7",
+    measurementId: "G-F8SCCQRP84"
+  };
+
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig); //must initialize app before calling the getting functions below
+  const analytics = getAnalytics(firebaseApp);
+  const auth = getAuth(firebaseApp);
+  const databse = getFirestore(firebaseApp);
+
+
+  //Detect auth state
+
+  onAuthStateChanged(auth, (user) => { //!! all subpackahes (datase, analytics etc) use this pattern
+	if (user !== null) {
+		console.log('use is logged in!')
+	} else {
+		console.log('user NOT logged in!')
+	}
+  });
