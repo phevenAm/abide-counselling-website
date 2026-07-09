e18e693ca106c76ad8259380f713619c59555a2d
Stephen <stephenmissah@outlook.com>
2026-07-09 12:15:05 +0100

    update withMe images + features

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,37 @@
-const CACHE = "static-v2";
+// const CACHE = "static-v10";
 
-self.addEventListener("install", e => {
-	e.waitUntil(
-		caches.open(CACHE).then(cache => {
-			return cache.addAll([
-				'./',
-				'./manifest.json',
-				'./public/css/main.min.css',
-				'./public/css/bootstrap.min.css',
-				'./public/scripts/main.min.js',
-				'./public/media/brand/fav/android-chrome-512x512.png',
-				'./public/media/brand/fav/android-chrome-192x192.png',
-				'./public/media/brand/fav/apple-touch-icon.png',
-				'./public/media/brand/fav/favicon-32x32.png',
-				'./public/media/images/portal/screenshot-progress.png',
-				'./public/media/images/portal/screenshot-resources.png',
-			]);
-		})
-	);
-});
+// self.addEventListener("install", e => {
+// 	e.waitUntil(
+// 		caches.open(CACHE).then(cache => {
+// 			return cache.addAll([
+// 				'./',
+// 				'./manifest.json',
+// 				'./public/css/main.min.css',
+// 				'./public/css/bootstrap.min.css',
+// 				'./public/scripts/main.min.js',
+// 				'./public/media/brand/fav/android-chrome-512x512.png',
+// 				'./public/media/brand/fav/android-chrome-192x192.png',
+// 				'./public/media/brand/fav/apple-touch-icon.png',
+// 				'./public/media/brand/fav/favicon-32x32.png',
+// 				'./public/media/images/portal/screenshot-progress.png',
+// 				'./public/media/images/portal/screenshot-resources.png',
+// 			]);
+// 		})
+// 	);
+// });
 
-self.addEventListener("activate", e => {
-	e.waitUntil(
-		caches.keys().then(keys =>
-			Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-		)
-	);
-});
+// self.addEventListener("activate", e => {
+// 	e.waitUntil(
+// 		caches.keys().then(keys =>
+// 			Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+// 		)
+// 	);
+// });
 
-self.addEventListener('fetch', e => {
-	e.respondWith(
-		caches.match(e.request).then(res => {
-			return res || fetch(e.request);
-		})
-	);
-});
+// self.addEventListener('fetch', e => {
+// 	e.respondWith(
+// 		caches.match(e.request).then(res => {
+// 			return res || fetch(e.request);
+// 		})
+// 	);
+// });
